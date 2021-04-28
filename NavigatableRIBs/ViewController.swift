5dcdaf1a2c2b103d1d26c7a8986195b10bab4638
comeonyoh <comeonyoh89@gmail.com>
2021-04-28 22:28:27 +0900

    Initial Commit

---
+++ NavigatableRIBs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigatableRIBs
+//
+//  Created by JungSu Kim on 2021/04/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
